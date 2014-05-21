126bde7907ae6197869a485d21462ec4e5cf2db6
twflu <yzzhou@thoughtworks.com>
2014-05-21 11:48:47 +0800

    Initial Commit

---
+++ MyToDo/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  MyToDo
+//
+//  Created by twer on 5/21/14.
+//  Copyright (c) 2014 twer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
